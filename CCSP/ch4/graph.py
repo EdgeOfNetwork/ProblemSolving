58bd481408d02edc41cd1a668df52d73cbc804fc
lsy <qhrrl4556@gmail.com>
2022-04-14 03:39:15 +0900

    weighted

--- CCSP/ch4/graph.py
+++ CCSP/ch4/graph.py
@@ -40,12 +40,14 @@ class Graph(Generic[V]):
     def index_of(self, vertex: V) -> int: #정점 인덱스를 찾는다.
         return self._vertices.index(vertex)
 
+    # _edges[index] 인접 리스트, 해당 인덱스의 정점이 다른 정점에 연결된 엣지리스트
     def neighbors_for_index(self, index: int) -> List[V]:
         return list(map(self.vertex_at, [e.v for e in self._edges[index]]))
 
     def neighbors_for_vertex(self, vertex: V) -> List[V]:
         return self.neighbors_for_index(self.index_of(vertex))
 
+    #
     def edges_for_index(self, index: int) -> List[Edge]:
         return self._edges[index]
 
@@ -55,6 +57,49 @@ class Graph(Generic[V]):
     def __str__(self) -> str:
         desc: str = ""
         for i in range(self.vertex_count):
-            desc += f"{self.vertex_at(i)} -> {self.neighbors_for_index(i)\n"
+            desc += f"{self.vertex_at(i)} -> {self.neighbors_for_index(i)}\n"
             return desc
 
+if __name__ == "__main__":
+    # test basic Graph construction
+    city_graph: Graph[str] = Graph(["Seattle", "San Francisco", "Los Angeles", "Riverside", "Phoenix", "Chicago", "Boston", "New York", "Atlanta", "Miami", "Dallas", "Houston", "Detroit", "Philadelphia", "Washington"])
+    city_graph.add_edge_by_vertices("Seattle", "Chicago")
+    city_graph.add_edge_by_vertices("Seattle", "San Francisco")
+    city_graph.add_edge_by_vertices("San Francisco", "Riverside")
+    city_graph.add_edge_by_vertices("San Francisco", "Los Angeles")
+    city_graph.add_edge_by_vertices("Los Angeles", "Riverside")
+    city_graph.add_edge_by_vertices("Los Angeles", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Chicago")
+    city_graph.add_edge_by_vertices("Phoenix", "Dallas")
+    city_graph.add_edge_by_vertices("Phoenix", "Houston")
+    city_graph.add_edge_by_vertices("Dallas", "Chicago")
+    city_graph.add_edge_by_vertices("Dallas", "Atlanta")
+    city_graph.add_edge_by_vertices("Dallas", "Houston")
+    city_graph.add_edge_by_vertices("Houston", "Atlanta")
+    city_graph.add_edge_by_vertices("Houston", "Miami")
+    city_graph.add_edge_by_vertices("Atlanta", "Chicago")
+    city_graph.add_edge_by_vertices("Atlanta", "Washington")
+    city_graph.add_edge_by_vertices("Atlanta", "Miami")
+    city_graph.add_edge_by_vertices("Miami", "Washington")
+    city_graph.add_edge_by_vertices("Chicago", "Detroit")
+    city_graph.add_edge_by_vertices("Detroit", "Boston")
+    city_graph.add_edge_by_vertices("Detroit", "Washington")
+    city_graph.add_edge_by_vertices("Detroit", "New York")
+    city_graph.add_edge_by_vertices("Boston", "New York")
+    city_graph.add_edge_by_vertices("New York", "Philadelphia")
+    city_graph.add_edge_by_vertices("Philadelphia", "Washington")
+    print(city_graph)
+
+    import sys
+    sys.path.insert(0, '..')
+    from ch2.generic_search import bfs, Node, node_to_path
+
+    bfs_result: Optional[Node[V]] = bfs("Boston", lambda x: x =="Miami",
+                                        city_graph.neighbors_for_vertex)
+    if bfs_result is None:
+        print("Cannot find path using BFS")
+    else:
+        path: List[V] = node_to_path(bfs_result)
+        print("보스턴에서 마이애미 최단 경로:")
+        print(path)
