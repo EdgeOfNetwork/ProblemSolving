a19d9c11c93a21605dff70d4e3356f584b2c41c7
lsy <qhrrl4556@gmail.com>
2022-06-10 21:59:14 +0900

    class 1

--- boj_py/Class/class1/2920.py
+++ boj_py/Class/class1/2920.py
@@ -1,14 +1,8 @@
 A = list(map(int, input().split()))
 
-cnt = 0
-for i in range(len(A) - 1):
-    if A[i] + 1 == A[i + 1] :
-        cnt += 1
-        if cnt == 7:
-            print("ascending")
-
-    elif A[i] - 1 == A[i + 1] :
-        cnt += 1
-        if cnt == 7:
-            print("decending")
-print("mixed")
+if A == sorted(A):
+    print("ascending")
+elif A == sorted(A, reverse=True):
+    print("descending")
+else:
+    print("mixed")
