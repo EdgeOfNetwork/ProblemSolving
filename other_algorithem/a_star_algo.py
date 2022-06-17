5598e5f8496774c72a4920504cb65048588ce508
lsy <qhrrl4556@gmail.com>
2022-06-17 22:42:07 +0900

    a_star_algo

---
+++ other_algorithem/a_star_algo.py
@@ -0,0 +1,16 @@
+matrix = [
+    [True, True, True, False, False, False, False],
+    [True, False, True, False, False, False, False],
+    [True, False, True, True, True, True, True],
+    [True, False, True, False, False, False, True],
+    [True, False, True, False, True, True, True],
+    [True, False, True, False, True, False, False],
+    [True, True, True, True, True, True, True],
+]
+
+
+
+Coord = tuple[int, int]
+
+start = (2, 2)
+dest = (6, 6)
